70f6905a10dbad668d280c077f3b57ac40f21ba6
Igor null <m1el@ya.ru>
2014-02-02 17:38:45 +0200

    fixed order of angles to compensate orientation

--- rotate.js
+++ rotate.js
@@ -1,20 +1,13 @@
 $(function() {
     $(window).on('deviceorientation', function(e) {
         var event = e.originalEvent,
-            tf1 = 
-                 'rotateY(' + event.gamma + 'deg)' +
-                 'rotateX(' + event.beta + 'deg) ' +
-                 'rotateZ(' + event.alpha + 'deg) ',
-            tf2 = 
-                 'rotateY(' + event.gamma + 'deg)' +
-                 'rotateX(' + event.beta + 'deg) ' +
-                 'rotateZ(' + event.alpha + 'deg) ' +
-                 'rotateX(90deg) ',
-            tf3 = 
-                 'rotateY(' + event.gamma + 'deg)' +
-                 'rotateX(' + event.beta + 'deg) ' +
-                 'rotateZ(' + event.alpha + 'deg) ' +
-                 'rotateY(90deg) ';
+            st = 
+                 'rotateY(' + (-event.gamma) + 'deg) ' +
+                 'rotateX(' + (event.beta) + 'deg)' +
+                 'rotateZ(' + (event.alpha-180) + 'deg) ',
+            tf1 = st + '',
+            tf2 = st + 'rotateX(90deg) ',
+            tf3 = st + 'rotateY(90deg) ';
         $('#rotateable1').css({
             '-webkit-transform': tf1
         });
